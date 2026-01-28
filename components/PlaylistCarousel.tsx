0408427b79b6e1ec1db886060b19ce5ac3088e70
Franco Tomassich <francotomassich@gmail.com>
2026-01-28 15:19:37 -0300

    margin changes

--- components/PlaylistCarousel.tsx
+++ components/PlaylistCarousel.tsx
@@ -40,6 +40,14 @@ export default function PlaylistCarousel() {
           Obras técnicas realizadas
         </h2>
 
+        <p
+  className="mt-6 mb-12 text-center text-lg sm:text-xl md:text-2xl text-muted-foreground leading-relaxed max-w-4xl mx-auto animate-fadeUp"
+  style={{ animationDelay: "0.7s" }}
+>
+  A continuación te mostramos algunos de nuestros proyectos en acción
+</p>
+
+
         {/* Carrusel con paginación */}
         <Swiper
           
